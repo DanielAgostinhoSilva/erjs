aacbed35fc94c6aa1f2d309d090612503abe66bb
Daniel <daniel.agostinho@hotmail.com>
2022-09-22 21:55:19 -0300

    feat: 2.30. Dica preciosa para lidar com formulários

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,47 @@
 import {useState} from "react";
 
 export default function Form() {
-    const [name, setName] = useState('Daniel')
-    return <div>
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        console.log(form)
+    }
+
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return <form onSubmit={handleSubmit}>
         <input
             placeholder="nome"
             type="text"
-            value={name}
-            onChange={event => setName(event.target.value)}
+            name={"name"}
+            value={form.name}
+            onChange={handleInputChange}
         />
-        <div>
-            {name}
-        </div>
-    </div>
+        <input
+            placeholder="e-mail"
+            type={'email'}
+            name={"email"}
+            value={form.email}
+            onChange={handleInputChange}
+        />
+        <input
+            placeholder="senha"
+            type={'password'}
+            name={"password"}
+            value={form.password}
+            onChange={handleInputChange}
+        />
+        <button type={"submit"}>Enviar Formulario</button>
+    </form>
 }
