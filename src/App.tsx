c678c5dbf7978a6003f5fa293257afab3cb6bb76
Daniel <daniel.agostinho@hotmail.com>
2022-09-22 21:24:01 -0300

    feat: 2.28. Manipulação de eventos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
-import Post from './components/Post';
 
 function App() {
-  const [post] = useState({
-    title: 'Título maneiro',
-    content: 'Lorem Ipsum dolor sit amet'
-  })
+    function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Card align="left" title={"Card"}>
-          <Post post={post} totalComments={12} />
-        </Card>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form action={'/teste'}>
+                    <button onClick={handleClick}>
+                        Clique em mim
+                    </button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
